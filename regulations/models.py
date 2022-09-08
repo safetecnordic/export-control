29c2ac290edefaad1f61c3000beff952ed5e8ff1
KatjaSi <katja.sivertsen@gmail.com>
2022-09-08 16:05:28 +0200

    created export control db and changes in models

--- regulations/models.py
+++ regulations/models.py
@@ -2,21 +2,21 @@ from django.db import models
 
 
 class Regulation(models.Model):
-    category = models.ForeignKey("Category")
-    sub_category = models.ForeignKey("SubCategory")
-    regime = models.ForeignKey("Regime")
+    category = models.ForeignKey("Category", on_delete=models.CASCADE)
+    sub_category = models.ForeignKey("SubCategory", on_delete=models.CASCADE)
+    regime = models.ForeignKey("Regime", on_delete=models.CASCADE)
 
     text = models.ManyToManyField("TextSection")
 
 
 class Category(models.Model):
     identifier = models.IntegerField()
-    name = models.CharField()
+    name = models.CharField(max_length=256)
 
 
 class SubCategory(models.Model):
-    identifier = models.CharField()
-    name = models.CharField()
+    identifier = models.CharField(max_length=256)
+    name = models.CharField(max_length=256)
 
 
 class Regime(models.Model):
@@ -25,5 +25,5 @@ class Regime(models.Model):
 
 class TextSection(models.Model):
     text = models.TextField()
-    parent = models.ForeignKey("TextSection", null=True)
+    parent = models.ForeignKey("TextSection", null=True, on_delete=models.CASCADE)
     note = models.BooleanField()
