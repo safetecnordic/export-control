016cfde9514ea86ff57b086100b871ee792aedc1
danielmursa <dani99gallo@gmail.com>
2022-09-09 11:56:15 +0200

    Some improvements

--- base/views.py
+++ base/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class PresentationView(TemplateView):
+    template_name = "base/presentation.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(PresentationView, self).get_context_data(**kwargs)
+        context["page_title"] = "ExportControl"
+        return context
--- exportcontrol/urls.py
+++ exportcontrol/urls.py
@@ -1,9 +1,9 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path
-from django.views.generic import RedirectView
+from base.views import PresentationView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", RedirectView.as_view(url=settings.HOMEPAGE), name="homepage"),
+    path("", PresentationView.as_view(), name="presentation"),
 ]
