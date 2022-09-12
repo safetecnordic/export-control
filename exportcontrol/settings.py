243f08c581ff04c78b2587c23de55c85feedd6e5
danielmursa <dani99gallo@gmail.com>
2022-09-12 11:25:59 +0200

    Create app

--- exportcontrol/settings.py
+++ exportcontrol/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_bootstrap5",
+    "accounts",
 ]
 
 MIDDLEWARE = [
@@ -89,6 +90,9 @@ STATIC_URL = "static/"
 
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "accounts.User"
+
 
 if DEBUG:
     INSTALLED_APPS.append("debug_toolbar")
--- exportcontrol/urls.py
+++ exportcontrol/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
 
 if settings.DEBUG:
