b01d84af54eef83db29643862e9c454bc9252a27
danielmursa <dani99gallo@gmail.com>
2022-09-12 13:31:55 +0200

    Configure i18n

--- base/views.py
+++ base/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import TemplateView
+from django.utils.translation import gettext as _
 
 
 class PresentationView(TemplateView):
@@ -6,5 +7,6 @@ class PresentationView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(PresentationView, self).get_context_data(**kwargs)
-        context["page_title"] = "ExportControl"
+        context["page_title"] = _("ExportControl")
+        context["page_description"] = _("This is a test to see if the translations work")
         return context
--- exportcontrol/urls.py
+++ exportcontrol/urls.py
@@ -1,13 +1,19 @@
-from base.views import PresentationView
+import django
 from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
+from django.urls import path, include
+from base.views import PresentationView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("i18n/", include(django.conf.urls.i18n)),
+]
+
+urlpatterns += i18n_patterns(
     path("accounts/", include("django.contrib.auth.urls")),
     path("", PresentationView.as_view(), name="presentation"),
-]
+)
 
 if settings.DEBUG:
     import debug_toolbar
