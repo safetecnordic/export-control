6fb69da4e55223d0a5e53dcad1feb35cd581ccc7
danielmursa <dani99gallo@gmail.com>
2022-09-12 12:51:17 +0200

    Change db_name

--- exportcontrol/settings.py
+++ exportcontrol/settings.py
@@ -63,7 +63,7 @@ WSGI_APPLICATION = "exportcontrol.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME", default="exportcontrol"),
+        "NAME": os.getenv("DB_NAME", default="postgres"),
         "USER": os.getenv("DB_USER", default="postgres"),
         "PASSWORD": os.getenv("DB_PASSWORD", default="postgres"),
         "HOST": os.getenv("DB_HOST", default="localhost"),
