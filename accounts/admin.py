7f2c05f83644d749c371b78505330f0bb521097c
danielmursa <dani99gallo@gmail.com>
2022-09-12 11:46:46 +0200

    Create user admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from accounts.models import User
 
-# Register your models here.
+
+class UserAdmin(UserAdmin):
+    model = User
+    list_display = (
+        "username",
+        "email",
+    )
+    search_fields = [
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+    ]
+    ordering = ("-date_joined",)
+    date_hierarchy = "date_joined"
+
+
+admin.site.register(User, UserAdmin)
