feab2e2c9afef6d3fb7994dfc8ecf72a96ea6ce4
danielmursa <dani99gallo@gmail.com>
2022-09-13 13:29:40 +0200

    Add (location("static/"),)

--- exportcontrol/settings.py
+++ exportcontrol/settings.py
@@ -97,6 +97,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATIC_URL = "/static/"
 STATIC_ROOT = location("public/static")
+STATICFILES_DIRS = (location("static/"),)
 
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
