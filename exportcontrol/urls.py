6e9afcde4089514d93ab145a6b2d52dd387c9de8
danielmursa <dani99gallo@gmail.com>
2022-09-16 16:18:37 +0200

    Add about us and contact us

--- exportcontrol/urls.py
+++ exportcontrol/urls.py
@@ -2,11 +2,14 @@ from base.views import PresentationView
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
     path("", PresentationView.as_view(), name="presentation"),
+    path("about-us/", TemplateView.as_view(template_name="base/about_us.html"), name="about_us"),
+    path("contact-us/", TemplateView.as_view(template_name="base/contact_us.html"), name="contact_us"),
 ]
 
 if settings.DEBUG:
