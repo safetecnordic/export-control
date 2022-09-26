d12e224dfbcbd103451c20b64779ce5f5a512f01
Jonas Sundt <jonassundt98@gmail.com>
2022-09-26 13:54:57 +0200

    fix adjust_indent.py

--- base/management/commands/adjust_indent.py
+++ base/management/commands/adjust_indent.py
@@ -2,8 +2,8 @@
 def main():
     text = ""
     newText = ""
-    with open('another_paragraph.txt',  'r', encoding="utf8" ) as f:
-            while(True):    
+    with open('Forskrift.txt',  'r', encoding="utf8" ) as f:
+            while(True):
                 line = f.readline()
                 if not line:
                     break
@@ -14,9 +14,20 @@ def main():
                 newText += " "*newLineSpacing + line.strip() + "\n"
     
     #write the newText to a file
-    f = open("DEMOadjusted.txt", "w")
+    f = open("DEMOforskrift.txt", "w")
     f.write(newText)
     f.close()
+
+
+def count_space(str):
+    # counter
+    count = 0
+    for i in str:
+        if i == " ":
+            count += 1   
+        else:
+            break
+    return count
 
 
 if __name__ == "__main__":
