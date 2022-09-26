8f78931d3436d19e8fbd24c58df29e0d287717aa
danielmursa <dani99gallo@gmail.com>
2022-09-26 14:24:44 +0200

    Create admin for regulations

--- regulations/admin.py
+++ regulations/admin.py
@@ -1,3 +1,56 @@
 from django.contrib import admin
+from regulations.models import *
 
-# Register your models here.
+
+class RegulationAdmin(admin.ModelAdmin):
+    model = Regulation
+    list_display = (
+        "category",
+        "sub_category",
+        "regime",
+        "regime_number",
+    )
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    model = Category
+    list_display = (
+        "identifier",
+        "name",
+        "part",
+    )
+
+
+class SubCategoryAdmin(admin.ModelAdmin):
+    model = Category
+    list_display = (
+        "identifier",
+        "name",
+    )
+
+
+class RegimeAdmin(admin.ModelAdmin):
+    model = Category
+    list_display = (
+        "name",
+        "number_range_max",
+        "number_range_min",
+    )
+
+
+class ParagraphAdmin(admin.ModelAdmin):
+    model = Category
+    list_display = (
+        "code",
+        "text",
+        "order",
+        "note",
+        "parent",
+    )
+
+
+admin.site.register(Regulation, RegulationAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(SubCategory, SubCategoryAdmin)
+admin.site.register(Regime, RegimeAdmin)
+admin.site.register(Paragraph, ParagraphAdmin)
