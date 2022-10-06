4ede0352d5bed1404706423be5494660905e19d5
KatjaSi <katja.sivertsen@gmail.com>
2022-10-06 15:02:41 +0200

    added pagination of search results

--- base/views.py
+++ base/views.py
@@ -2,8 +2,9 @@ from django.conf import settings
 from django.shortcuts import render
 from django.utils.translation import gettext as _
 from django.views.generic import TemplateView
-from regulations.models import Category, Paragraph, SubCategory, Regime
+from django.core.paginator import Paginator
 from django.contrib.postgres.search import SearchQuery, SearchVector, SearchRank, SearchHeadline
+from regulations.models import Category, Paragraph, SubCategory, Regime
 #TemplateView should be used when you want to present some information on an HTML page. TemplateView shouldn’t 
 #be used when your page has forms and does creation or update of objects. In such cases, 
 #FormView, CreateView, or UpdateView is a better option.
@@ -13,20 +14,26 @@ def search(request):
     regimes = Regime.objects.all()
     q = request.GET.get('q')
     if q:
+        print(q)
         vector = SearchVector('text')
         query = SearchQuery(q)
         search_headline = SearchHeadline('text', query) 
         paragraphs = Paragraph.objects. \
             annotate(rank = SearchRank(vector, query)).annotate(headline = search_headline).filter(rank__gte=0.001).order_by('-rank')
+
+        paginator = Paginator(paragraphs, 10)
+        page = request.GET.get('page')
+        paged_paragraphs = paginator.get_page(page)
+
     else:
-        paragraphs = None
+        paged_paragraphs = None
     context = dict()
     context["page_title"] = settings.SITE_NAME
     context["page_description"] = _("This is a test to see if the translations work")
     context["categories"] = categories
     context["subcategories"] = subcategories
     context["regimes"] = regimes
-    context['paragraphs'] = paragraphs
+    context['paragraphs'] = paged_paragraphs
     context['q'] = q
     return render(request, "base/presentation.html", context)
 
