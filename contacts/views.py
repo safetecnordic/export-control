d4ff40c968b70d6fd9fa06ea55065dedde796212
KatjaSi <katja.sivertsen@gmail.com>
2022-10-12 09:49:31 +0200

    saving contact and confirmation message #57

--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from concurrent.futures.process import _python_exit
+from django.shortcuts import redirect, render
+
+from django.contrib import messages
+from contacts.models import Contact
 
 def contact(request):
     context = dict()
-    return render(request, 'contact_form.html', context)
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        message = request.POST['message']
+
+        contact = Contact(name=name, email=email, phone=phone, message=message)
+        contact.save()
+
+        messages.success(request, "Thank you for your request, we get back to you soon!")
+        return redirect(request.META.get('HTTP_REFERER'))
+
+
