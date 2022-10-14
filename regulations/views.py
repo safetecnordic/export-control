ebd1c5f9786420f9ffa1d3bca09eba697867f56a
danielmursa <dani99gallo@gmail.com>
2022-10-14 16:02:15 +0200

    Fix view

--- regulations/views.py
+++ regulations/views.py
@@ -16,24 +16,27 @@ class SearchView(ListView):
     model = Paragraph
     paginate_by = 20
     template_name = "regulations/search_page.html"
+    form = SearchForm()
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["search_term"] = self.request.GET.get("q", "")
         context["search_form"] = SearchForm(self.request.GET)
+        context["search_term"] = self.request.GET.get("q", "")
         context["page_title"] = _("Search")
         return context
 
     def get_queryset(self):
-        q = self.request.GET.get("q", "")
-        paragraphs = search_paragraphs(q) if q else Paragraph.get_root_nodes()
-        paragraphs = filter_paragraphs(
-            paragraphs,
-            self.request.GET.get("category", None),
-            self.request.GET.get("subcategory", None),
-            self.request.GET.get("regime", None),
-        )
-
+        self.form = SearchForm(self.request.GET)
+        paragraphs = list()
+        if self.form.is_valid():
+            q = self.request.GET.get("q", "")
+            paragraphs = search_paragraphs(q) if q else Paragraph.get_root_nodes()
+            paragraphs = filter_paragraphs(
+                paragraphs,
+                self.request.GET.get("category", None),
+                self.request.GET.get("subcategory", None),
+                self.request.GET.get("regime", None),
+            )
         return paragraphs
 
 
@@ -45,7 +48,5 @@ class RegulationDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["page_title"] = settings.SITE_NAME
-        context["page_description"] = _("This is a test to see if the translations work")
         context["root_paragraphs"] = self.object.paragraphs.filter(depth=1)
         return super(RegulationDetailView, self).get_context_data(**kwargs)
