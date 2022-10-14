645efbfda194f402ea02076399ea07b0511b54d3
KatjaSi <katja.sivertsen@gmail.com>
2022-10-14 11:50:17 +0200

    headbar fix, form styling #57

--- contacts/forms.py
+++ contacts/forms.py
@@ -7,9 +7,16 @@ class ContactForm(forms.ModelForm):
         model = Contact
         fields = ["name", "email", "phone", "message"]
 
+        labels = {
+            'name': '',
+            'email': '',
+            'phone': '',
+            'message': '',
+        }
+
         widgets = {
-            'name': forms.TextInput(attrs={'class': 'form-control'}),
-            'email': forms.TextInput(attrs={'class': 'form-control'}),
-            'phone': forms.TextInput(attrs={'class': 'form-control'}),
-            'message': forms.Textarea(attrs={'class': 'form-control'})
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Name'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder':'Email'}),
+            'phone': forms.TextInput(attrs={'class': 'form-control', 'placeholder':'Phone'}),
+            'message': forms.Textarea(attrs={'class': 'form-control','placeholder':'Enter your message here'})
         }
