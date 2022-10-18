3a881aa1b10cec8d0d254b0be99421ab38145aa5
Kamomilla <kmgodlun@stud.ntnu.no>
2022-10-18 15:08:50 +0200

    Responsive header

--- base/static/base/js/main.js
+++ base/static/base/js/main.js
@@ -1,3 +1,22 @@
-setTimeout(function() {
-    $('#message').fadeOut(4000);
-}, 2500)
+// const toggleButton = document.getElementsByClassName("toggle-button");
+// const navbarLinks = document.getElementsByClassName("navbar-links");
+// const hei = "hei :)";
+
+// console.log("hello");
+// console.log(toggleButton);
+// console.log(navbarLinks);
+
+// console.log(toggleButton.item(0));
+// console.log(document.body);
+
+// document.addEventListener("DOMContentLoaded", function (event) {
+//   console.log("DOM fully loaded and parsed");
+//   console.log(toggleButton.item(0));
+//   console.log(document.body);
+
+//   if (toggleButton.item(0)) {
+//     toggleButton.item(0).addEventListener("click", () => {
+//       navbarLinks.classList.toggle("active");
+//     });
+//   }
+// });
