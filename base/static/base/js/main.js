072b908da42c823474d18bf0f9d53c11ac282e6a
Kamomilla <kmgodlun@stud.ntnu.no>
2022-10-20 11:50:24 +0200

    Added function to main.js

--- base/static/base/js/main.js
+++ base/static/base/js/main.js
@@ -1,11 +1,3 @@
-// const toggleButton = document.getElementsByClassName("toggle-button");
-// const navbarLinks = document.getElementsByClassName("navbar-links");
-
-// document.addEventListener("DOMContentLoaded", function (event) {
-
-//   if (toggleButton.item(0)) {
-//     toggleButton.item(0).addEventListener("click", () => {
-//       navbarLinks.classList.toggle("active");
-//     });
-//   }
-// });
+setTimeout(function () {
+  $("#message").fadeOut(4000);
+}, 2500);
