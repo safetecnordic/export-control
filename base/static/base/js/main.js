8b29c1df1ffa827c562b378473186b2bf04cfa90
Kamomilla <kmgodlun@stud.ntnu.no>
2022-10-20 11:39:17 +0200

    Removed some comments

--- base/static/base/js/main.js
+++ base/static/base/js/main.js
@@ -1,18 +1,7 @@
 // const toggleButton = document.getElementsByClassName("toggle-button");
 // const navbarLinks = document.getElementsByClassName("navbar-links");
-// const hei = "hei :)";
-
-// console.log("hello");
-// console.log(toggleButton);
-// console.log(navbarLinks);
-
-// console.log(toggleButton.item(0));
-// console.log(document.body);
 
 // document.addEventListener("DOMContentLoaded", function (event) {
-//   console.log("DOM fully loaded and parsed");
-//   console.log(toggleButton.item(0));
-//   console.log(document.body);
 
 //   if (toggleButton.item(0)) {
 //     toggleButton.item(0).addEventListener("click", () => {
