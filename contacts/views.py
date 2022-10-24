a5182d435a833d77a231dcf9cf3293d509d8b0b5
danielmursa <dani99gallo@gmail.com>
2022-10-24 16:44:30 +0200

    Improvements on template

--- contacts/views.py
+++ contacts/views.py
@@ -14,5 +14,5 @@ class AddContactView(CreateView):
 
     def get_context_data(self, *args, **kwargs):
         context = super(AddContactView, self).get_context_data(**kwargs)
-        context["page_title"] = _("Consult Safetech")
+        context["page_title"] = _("Consult Safetec")
         return context
