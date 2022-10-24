cbc3f277b3efe64f294bb1c29da8787149b48298
danielmursa <dani99gallo@gmail.com>
2022-10-24 14:14:56 +0200

    Improve Search

--- regulations/search.py
+++ regulations/search.py
@@ -1,32 +1,60 @@
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, SearchHeadline
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchHeadline
 from treebeard.mp_tree import MP_NodeManager
-from regulations.models import Paragraph
+from django.db.models import Q
 
 
-def get_searched_paragraphs(search_term: str) -> MP_NodeManager:
+def get_search_queries(as_q, as_qand, as_qor, as_qnot):
+    query = None
+    not_query = None
+    and_query = None
+    or_query = None
+    if as_q:
+        query = SearchQuery(as_q)
+    if as_qand:
+        and_query = SearchQuery(as_qand, search_type="phrase")
+    if as_qor:
+        if as_q:
+            as_qor = f"{as_q} {as_qor}"
+        or_query = SearchQuery(as_qor, search_type="websearch")
+    if as_qnot:
+        not_query = SearchQuery(as_qnot, search_type="websearch")
+    return query, and_query, or_query, not_query
+
+
+def get_searched_paragraphs(search_terms: dict, paragraphs: MP_NodeManager) -> MP_NodeManager:
     field_to_search = "text"
-    search_term = search_term.lower()
-    breakpoint()
     search_vector = SearchVector(field_to_search)
-    # search_query = SearchQuery(search_term, search_type="phrase")
-    search_query = SearchQuery(search_term)
-    search_headline = SearchHeadline(field_to_search, search_query)
-    paragraphs = (
-        Paragraph.objects.annotate(headline=search_headline).annotate(search=search_vector).filter(search=search_query)
+    query, and_query, or_query, not_query = get_search_queries(
+        search_terms["as_q"] if "as_q" in search_terms.keys() else None,
+        search_terms["as_qand"] if "as_qand" in search_terms.keys() else None,
+        search_terms["as_qor"] if "as_qor" in search_terms.keys() else None,
+        search_terms["as_qnot"] if "as_qnot" in search_terms.keys() else None,
     )
+    search_headline = SearchHeadline(field_to_search, query)
+    paragraphs = paragraphs.annotate(search=search_vector).annotate(headline=search_headline)
+    if query:
+        paragraphs = paragraphs.filter(search=query)
+    if and_query:
+        search_headline = SearchHeadline(field_to_search, and_query)
+        paragraphs = paragraphs.annotate(headline=search_headline).filter(search=and_query)
+    if or_query:
+        search_headline = SearchHeadline(field_to_search, or_query)
+        paragraphs = paragraphs.annotate(headline=search_headline).filter(search=or_query)
+    if not_query:
+        paragraphs = paragraphs.exclude(search=not_query)
+
     return paragraphs
 
 
-def filter_paragraphs(
-    paragraphs: MP_NodeManager,
-    category_id: int | None,
-    subcategory_id: int | None,
-    regime_id: int | None,
-) -> MP_NodeManager:
-    if category_id:
-        paragraphs = paragraphs.filter(regulation__category__id=category_id)
-    if subcategory_id:
-        paragraphs = paragraphs.filter(regulation__sub_category__id=subcategory_id)
-    if regime_id:
-        paragraphs = paragraphs.filter(regulation__regime__id=regime_id)
-    return paragraphs
+def get_filtered_paragraphs(search_terms: dict, paragraphs: MP_NodeManager) -> MP_NodeManager:
+    if "as_cat" in search_terms.keys() and search_terms["as_cat"]:
+        paragraphs = paragraphs.filter(
+            Q(category=search_terms["as_cat"]) | Q(regulation__category=search_terms["as_cat"])
+        )
+    if "as_subcat" in search_terms.keys() and search_terms["as_subcat"]:
+        paragraphs = paragraphs.filter(
+            Q(sub_category=search_terms["as_subcat"]) | Q(regulation__sub_category=search_terms["as_subcat"])
+        )
+    if "as_reg" in search_terms.keys() and search_terms["as_reg"]:
+        paragraphs = paragraphs.filter(regulation__regime=search_terms["as_reg"])
+    return paragraphs.filter(note_type=search_terms["as_type"])
