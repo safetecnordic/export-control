1c5201c5f0c56abad3d63b00e7aace22193c501a
danielmursa <dani99gallo@gmail.com>
2022-10-24 16:42:39 +0200

    Add Test file

--- contacts/tests.py
+++ contacts/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.utils.translation import gettext as _
 
-# Create your tests here.
+from contatcs.models import Contact
+
+
+class ContactTests(TestCase):
+    def test_search_view(self):
+        url = reverse("contact_add")
+        response = self.client.get(url, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, _("Consult Safetec"))
+
+    """    def test_search_form(self):
+        form_data = {"as_q": "Hydrogen", "as_cat": "TEST"}
+        form = SearchForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertEqual(
+            form.errors["as_cat"][0], "Select a valid choice. That choice is not one of the available choices."
+        )
+        form_data = {"as_q": "Hydrogen", "as_cat": 1}
+        form = SearchForm(data=form_data)
+        self.assertTrue(form.is_valid())"""
