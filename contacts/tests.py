68e4d322d0ee6f868d50b527f90b7eb4341b77fd
danielmursa <dani99gallo@gmail.com>
2022-10-25 09:50:01 +0200

    Create tests for Contact

--- contacts/tests.py
+++ contacts/tests.py
@@ -2,23 +2,67 @@ from django.test import TestCase
 from django.urls import reverse
 from django.utils.translation import gettext as _
 
-from contatcs.models import Contact
+from contacts.models import Contact
 
 
 class ContactTests(TestCase):
-    def test_search_view(self):
-        url = reverse("contact_add")
+    def test_add_contat_view(self):
+        url = reverse("add_contact")
         response = self.client.get(url, follow=True)
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, _("Consult Safetec"))
 
-    """    def test_search_form(self):
-        form_data = {"as_q": "Hydrogen", "as_cat": "TEST"}
-        form = SearchForm(data=form_data)
-        self.assertFalse(form.is_valid())
-        self.assertEqual(
-            form.errors["as_cat"][0], "Select a valid choice. That choice is not one of the available choices."
+        # test form
+        contacts = Contact.objects.all()
+        self.assertEqual(contacts.count(), 0)
+
+        response = self.client.post(
+            url,
+            {
+                "name": "",
+                "email": "",
+                "phone_number": "",
+                "message": "",
+            },
         )
-        form_data = {"as_q": "Hydrogen", "as_cat": 1}
-        form = SearchForm(data=form_data)
-        self.assertTrue(form.is_valid())"""
+        self.assertFormError(response, "form", "name", "This field is required.")
+        self.assertFormError(response, "form", "email", "This field is required.")
+        self.assertFormError(response, "form", "message", "This field is required.")
+
+        contacts = Contact.objects.all()
+        self.assertEqual(contacts.count(), 0)
+
+        response = self.client.post(
+            url,
+            {
+                "name": "Test",
+                "email": "",
+                "phone_number": "",
+                "message": "",
+            },
+        )
+        self.assertFormError(response, "form", "email", "This field is required.")
+        self.assertFormError(response, "form", "message", "This field is required.")
+
+        contacts = Contact.objects.all()
+        self.assertEqual(contacts.count(), 0)
+        response = self.client.post(
+            url,
+            {
+                "name": "Test",
+                "email": "test@test.come",
+                "phone_number": "+39324587980",
+                "message": "Add here some text",
+            },
+        )
+        success_url = reverse("thank_you")
+        self.assertRedirects(response, success_url)
+        self.assertContains(response, _("Thank you"))
+
+        contacts = Contact.objects.all()
+        contact = contacts.first()
+        self.assertEqual(contacts.count(), 1)
+        self.assertEqual(contact.message, "Add here some text")
+        self.assertEqual(contact.name, "Test")
+        self.assertEqual(contact.email, "test@test.come")
+        self.assertEqual(contact.phone_number, "+39324587980")
