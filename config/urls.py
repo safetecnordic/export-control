cea0a2f7061cc242fce072d699fa7c2cea0f4303
KatjaSi <katja.sivertsen@gmail.com>
2022-10-27 11:25:31 +0200

    flat page related

--- config/urls.py
+++ config/urls.py
@@ -8,25 +8,24 @@ from django.conf.urls.i18n import i18n_patterns
 from django.urls import path, include
 from base.views import FrontPageView
 from django.contrib.flatpages import views
-
 from config.views import error_404
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("home/", error_404),   
-    path("consult/", error_404),    
-    path("i18n/", include(django.conf.urls.i18n)),
-]+static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    path("i18n/", include('django.conf.urls.i18n')),
+] +static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
 urlpatterns += i18n_patterns(
+    path("home/", error_404),   
+    path("consult/", error_404),    
     path("regulations/", include("regulations.urls")),
     path("contacts/", include("contacts.urls")),
     path("export/", TemplateView.as_view(template_name="base/export_control_law.html"), name="export_control_law"),
     path('knowledge-transfer/', views.flatpage, {'url': '/knowledge-transfer/'}, name="knowledge_transfer"),
     #path("knowledge-transfer/", TemplateView.as_view(template_name="base/knowledge_transfer.html"), name="knowledge_transfer"),
-    path("home/", error_404),    
     path("", FrontPageView.as_view(), name="front_page"),
+    #prefix_default_language=False,
 )
 
 if settings.DEBUG:
