6ecc944463ef31a0f25105f498bfc8a04da2d385
danielmursa <dani99gallo@gmail.com>
2022-10-31 13:51:42 +0100

    Improve urls

--- config/urls.py
+++ config/urls.py
@@ -20,10 +20,7 @@ urlpatterns += i18n_patterns(
     path("consult/", handler404),
     path("regulations/", include("regulations.urls")),
     path("contacts/", include("contacts.urls")),
-    # path("export/", TemplateView.as_view(template_name="base/export_control_law.html"), name="export_control_law"),
-    path("knowledge-transfer/", views.flatpage, {"url": "/knowledge-transfer/"}, name="knowledge_transfer"),
-    path("export-control-law/", views.flatpage, {"url": "/export/"}, name="export_control_law"),
-    # path("knowledge-transfer/", TemplateView.as_view(template_name="base/knowledge_transfer.html"), name="knowledge_transfer"),
+    path("", include("django.contrib.flatpages.urls")),
     path("", FrontPageView.as_view(), name="front_page"),
 )
 
