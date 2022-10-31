53e0586c12781b290c2fc384af1f5e80682cc9ef
danielmursa <dani99gallo@gmail.com>
2022-10-31 15:55:31 +0100

    Delete images

--- base/admin.py
+++ base/admin.py
@@ -7,17 +7,39 @@ from django.utils.translation import gettext_lazy as _
 # Define a new FlatPageAdmin
 class FlatPageAdmin(FlatPageAdmin):
     fieldsets = (
-        (None, {'fields': ('url', 'title', 'title_no', 'title_description','title_description_no', 
-                'page_content', 'page_content_no', 'sidebar_title', 'sidebar_title_no', 'sidebar_text', 'sidebar_text_no', 'sites', 'image')}),
-        (_('Advanced options'), {
-            'classes': ('collapse',),
-            'fields': (
-                'enable_comments',
-                'registration_required',
-                'template_name',
-            ),
-        }),
+        (
+            None,
+            {
+                "fields": (
+                    "url",
+                    "title",
+                    "title_no",
+                    "title_description",
+                    "title_description_no",
+                    "page_content",
+                    "page_content_no",
+                    "sidebar_title",
+                    "sidebar_title_no",
+                    "sidebar_text",
+                    "sidebar_text_no",
+                    "sites",
+                    "image",
+                )
+            },
+        ),
+        (
+            _("Advanced options"),
+            {
+                "classes": ("collapse",),
+                "fields": (
+                    "enable_comments",
+                    "registration_required",
+                    "template_name",
+                ),
+            },
+        ),
     )
+
 
 # Re-register FlatPageAdmin
 admin.site.unregister(FlatPage)
