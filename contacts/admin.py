a71544c75bb12868750d7ec32f3a90f6f4fff79e
danielmursa <dani99gallo@gmail.com>
2022-11-03 11:44:05 +0100

    Improve contacts admin

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.core.exceptions import PermissionDenied
 
 from .models import Contact
 
@@ -9,16 +10,24 @@ class ContactAdmin(admin.ModelAdmin):
         "name",
         "email",
         "phone_number",
-        "message",
         "date_created",
-        "status",
+        "get_status_display_in_admin",
         "archived",
     )
     list_filter = (
         "status",
         "archived",
     )
+    actions = [
+        "set_archived",
+    ]
     search_fields = ("name", "email", "phone_number")
+
+    def set_archived(self, request, queryset):
+        if not self.has_change_permission(request):
+            raise PermissionDenied
+        Contact.objects.filter(pk__in=queryset.values_list("pk", flat=True)).update(archived=True)
+        self.message_user(request, "The contacts has been updated")
 
 
 admin.site.register(Contact, ContactAdmin)
